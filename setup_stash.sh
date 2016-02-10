63c7f4762cc7956afe6887a60cb3035acff00437
Nate McCurdy <nate.mccurdy@puppetlabs.com>
2016-02-09 19:29:34 -0800

    Remove setup_stash.sh

--- setup_stash.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-stash_url='http://10.20.1.23:7990/'
-creds='admin:admin'
-
-curl -u $creds -X POST "${stash_url}/rest/api/1.0/admin/users?name=r10k&password=puppet&displayName=r10k&emailAddress=r10k@puppet.com"
